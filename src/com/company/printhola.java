b27a5f8aeb329f12ae349c5fd66d906f4b186d84
Joan Piza Ferra <pizajoan97@gmail.com>
2016-11-09 21:53:04 +0100

    afegim un sout per github

---
+++ src/com/company/printhola.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by jpizaf on 03/11/16.
+ */
+public class printhola {
+    public static void printHola(){
+        System.out.println("Hola món!");
+        System.out.println("Això s'ha afegit per Github!");
+    }
+}
