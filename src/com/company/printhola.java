c26a0eac074644832af5f7cfbe9bd0fec8b9262b
Joan Piza Ferra <pizajoan97@gmail.com>
2016-11-10 18:11:15 +0100

    nova impresio per pantalla

--- src/com/company/printhola.java
+++ src/com/company/printhola.java
@@ -1,5 +1,4 @@
 package com.company;
-
 /**
  * Created by jpizaf on 03/11/16.
  */
@@ -7,5 +6,6 @@ public class printhola {
     public static void printHola(){
         System.out.println("Hola món!");
         System.out.println("Això s'ha afegit per Github!");
+        System.out.println("Canvi de la branca test");
     }
 }
