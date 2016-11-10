fc764ace79450c90cb96e328b077e9c2fec84bae
Joan Piza Ferra <pizajoan97@gmail.com>
2016-11-10 21:28:00 +0100

    borram linia

--- src/com/company/printhola.java
+++ src/com/company/printhola.java
@@ -6,6 +6,5 @@ public class printhola {
     public static void printHola(){
         System.out.println("Hola món!");
         System.out.println("Això s'ha afegit per Github!");
-        System.out.println("Canvi de la branca test");
     }
 }
